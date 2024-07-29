81615827523ab26085972214206e81a2ff4d9d70
DiyaShajith <diyashajith.2231@gmail.com>
2024-07-29 10:33:17 +0530

    foodmenu listview builder

--- lib/listviewex/foodmenu.dart
+++ lib/listviewex/foodmenu.dart
@@ -8,49 +8,142 @@ class Foodmenu extends StatefulWidget {
 }
 
 class _FoodmenuState extends State<Foodmenu> {
+  List images = [
+    "assets/foodcart/Cookie.webp",
+    "assets/foodcart/noodles.webp",
+    "assets/foodcart/Kabab.webp",
+    "assets/foodcart/Tea.webp"
+  ];
+  List names = ["Snacks", "Chinese", "Chinese", "Drinks"];
+  List title = [
+    "Sweetmeat French Dessert",
+    "Chicken with Spicy Noodles",
+    "Crispy Buffalo Chicken Wings",
+    "Spiced Chai Cafe"
+  ];
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "FoodCart",
-          style: TextStyle(
-              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 25),
+        appBar: AppBar(
+          title: Text(
+            "FoodCart",
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 25),
+          ),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Stack(
-          children: [
-            Container(
-              width: double.infinity,
-              height: size.height * 0.35,
-            ),
-            Positioned(
-              bottom: 0,
-              left: 10,
-              right: 10,
-              child: Container(
-                width: size.width * 0.8,
+        body: ListView.builder(
+          itemCount: images.length,
+          padding: EdgeInsets.all(16),
+          itemBuilder: (context, index) => Stack(
+            children: [
+              Container(
+                width: double.infinity,
                 height: size.height * 0.25,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(width: 2, color: Colors.black)),
               ),
-            ),
-            Positioned(
-                top: 20,
-                child: Image(
-                  image: AssetImage(
-                    "assets/foodcart/foodcart1.webp",
+              Positioned(
+                bottom: 0,
+                left: 10,
+                right: 10,
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  width: size.width * 0.8,
+                  height: size.height * 0.18,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(width: 2, color: Colors.black)),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 120,
+                      ),
+                      Column(
+                        children: [
+                          IntrinsicHeight(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                VerticalDivider(
+                                  color: Colors.orange,
+                                  thickness: 3,
+                                ),
+                                Text(
+                                  names[index],
+                                  style: TextStyle(
+                                      color: Colors.orange,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(
+                                  width: 70,
+                                ),
+                                Icon(
+                                  Icons.share,
+                                  color: Colors.blueGrey,
+                                  size: 30,
+                                ),
+                                Icon(
+                                  Icons.add_circle_outline_sharp,
+                                  color: Colors.orange,
+                                  size: 30,
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 210,
+                            child: Text(
+                              title[index],
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 25),maxLines: 2,overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Icon(
+                                Icons.emoji_food_beverage_outlined,
+                                color: Colors.grey,
+                              ),
+                              Text(
+                                "50 min",
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(
+                                width: 60,
+                              ),
+                              Icon(
+                                Icons.child_friendly,
+                                color: Colors.grey,
+                              ),
+                              Text(
+                                "8 lng",
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
                   ),
-                  width: 180,
-                  height: 180,
-                ))
-          ],
-        ),
-      ),
-    );
+                ),
+              ),
+              Positioned(
+                  top: 10,
+                  child: Image(
+                    image: AssetImage(images[index]),
+                    width: 130,
+                    height: 150,
+                  ))
+            ],
+          ),
+        ));
   }
 }
